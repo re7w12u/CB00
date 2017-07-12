ae9cb2248d7aef0ab26b333472790843e073a133
syntax-error <syntax-error@live.com>
2017-07-12 15:17:53 +0200

    init

---
+++ Assets/Scripts/CubeSelection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSelection : MonoBehaviour
+{
+
+    public Texture2D blue, red;
+
+    public void TriggerIn()
+    {
+        GetComponent<Renderer>().material.mainTexture = red;
+    }
+
+
+    public void TriggerOut()
+    {
+        GetComponent<Renderer>().material.mainTexture = blue;
+    }
+
+
+}
